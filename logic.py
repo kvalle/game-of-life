ae74b5766cf43a68d09a9824c2d237dc6bd6f4f3
Kjetil Valle <kjetil.valle@gmail.com>
2014-03-07 10:27:13 +0100

    fetching values from board

--- logic.py
+++ logic.py
@@ -5,3 +5,11 @@ def neighbours(pos):
     return [(x,y) for x in range(pos[0]-1,pos[0]+2)
                     for y in range(pos[1]-1, pos[1]+2)
                         if x != pos[0] or y != pos[1]]
+
+def value(board, p):
+    x = p[0] % len(board)
+    y = p[1] % len(board[0])
+    return board[x][y]
+
+def values(board, ps):
+    return [value(board, p) for p in ps]
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,17 +3,16 @@
 from nose.tools import assert_equals, assert_raises_regexp
 from os.path import dirname, relpath, join
 
-import board
 import logic
 
-board_file = join(dirname(relpath(__file__)), 'board.txt')
-example = board.from_file(board_file)
-
 def test_get_members():
-    members = logic.members(example)
-    assert_equals(70, len(members))
+    b = [[0,1,1,1],
+         [0,0,0,1],
+         [1,1,1,1]]
+    members = logic.members(b)
+    assert_equals(12, len(members))
     assert_equals((0,0), members[0])
-    assert_equals((6,9), members[-1])
+    assert_equals((2,3), members[-1])
 
 def test_get_neighbours():
     expected = [
@@ -22,3 +21,31 @@ def test_get_neighbours():
         (6,1), (6,2), (6,3)
     ]
     assert_equals(expected, logic.neighbours((5,2)))
+
+def test_get_value_from_board():
+    b = [[0,0,0],
+         [0,0,0],
+         [0,0,1]]
+    assert_equals(0, logic.value(b, (0,0)))
+    assert_equals(1, logic.value(b, (2,2)))
+
+def test_get_value_wraps():
+    b = [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,1,0],
+    ]
+
+    assert_equals(1, logic.value(b, (2,2))) 
+    assert_equals(1, logic.value(b, (2,6)))
+    assert_equals(1, logic.value(b, (5,2)))
+    assert_equals(1, logic.value(b, (-1,-2)))
+
+def test_get_values():
+    b = [
+        [0,0,1],
+        [0,1,1],
+    ]
+
+    assert_equals([0,0,1,0,1,1], 
+        logic.values(b, [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]))
