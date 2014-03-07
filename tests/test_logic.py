dd8a55f5572c7e8a004ab2735de2d3fb899ea5aa
Kjetil Valle <kjetil.valle@gmail.com>
2014-03-07 10:54:24 +0100

    cleanup

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -15,11 +15,9 @@ def test_get_members():
     assert_equals((2,3), members[-1])
 
 def test_get_neighbours():
-    expected = [
-        (4,1), (4,2), (4,3),
-        (5,1),        (5,3),
-        (6,1), (6,2), (6,3)
-    ]
+    expected = [(4,1), (4,2), (4,3),
+                (5,1),        (5,3),
+                (6,1), (6,2), (6,3)]
     assert_equals(expected, logic.neighbours((5,2)))
 
 def test_get_value_from_board():
@@ -30,22 +28,21 @@ def test_get_value_from_board():
     assert_equals(1, logic.value(b, (2,2)))
 
 def test_get_value_wraps():
-    b = [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,1,0],
-    ]
-
+    b = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,1,0]]
     assert_equals(1, logic.value(b, (2,2))) 
     assert_equals(1, logic.value(b, (2,6)))
     assert_equals(1, logic.value(b, (5,2)))
     assert_equals(1, logic.value(b, (-1,-2)))
 
 def test_get_values():
-    b = [
-        [0,0,1],
-        [0,1,1],
-    ]
-
+    b = [[0,0,1],
+         [0,1,1]]
     assert_equals([0,0,1,0,1,1], 
         logic.values(b, [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]))
+
+def test_next_board():
+    b = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,1,0]]
