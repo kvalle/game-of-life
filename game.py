bfbf44bf3e47d980b72137e399374012ffe0a267
Kjetil Valle <kjetil.valle@gmail.com>
2014-03-22 08:43:38 +0100

    changed name of some arguments

--- game.py
+++ game.py
@@ -8,8 +8,8 @@ import logic, board
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Conway`s Game of Life')
     parser.add_argument('boardfile', type=str)
-    parser.add_argument('-g', '--gens', type=int, help='maximum number of generations (default 10)', default=10)
-    parser.add_argument('-s', '--sleep', type=int, help='sleep time (default 200 ms)', default=200)
+    parser.add_argument('-g', '--max-gens', type=int, help='maximum number of generations (default: 10)', default=10)
+    parser.add_argument('-s', '--sleep-time', type=int, help='sleep time (default: 200 ms)', default=200)
     args = parser.parse_args()
 
     try:
@@ -18,7 +18,7 @@ if __name__=='__main__':
         print "Could not find board file:", sys.argv[1]
         sys.exit(1)
 
-    for gen in logic.generations(b, args.gens):
+    for gen in logic.generations(b, args.max_gens):
         os.system('cls' if os.name == 'nt' else 'clear')
         print board.to_string(gen)
-        time.sleep(args.sleep / 1000.)
+        time.sleep(args.sleep_time / 1000.)
